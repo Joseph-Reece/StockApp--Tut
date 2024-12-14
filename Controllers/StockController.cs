abde358ed1c25134c45d2af4021a47a97b71d66f
Ndirangu <ndirangujkariuki@gmail.com>
2024-12-14 03:09:48 +0300

    Add **Dto** (Data Transfer Object) to Select the columns we need to transfer

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -20,7 +22,8 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList()
+            .Select(x => x.ToStockDto());
             return Ok(stocks);
         }
 
@@ -32,7 +35,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
         
     }
